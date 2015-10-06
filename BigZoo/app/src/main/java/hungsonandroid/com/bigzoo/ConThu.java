c34722ad90fe0f6c92b46507857f63186bd7efbd
sondevelopervn <hungson12007@gmail.com>
2015-10-06 13:23:50 +0700

    Son update
    son update

---
+++ BigZoo/app/src/main/java/hungsonandroid/com/bigzoo/ConThu.java
@@ -0,0 +1,14 @@
+package hungsonandroid.com.bigzoo;
+
+/**
+ * Created by Administrator on 05.10.2015.
+ */
+public class ConThu  {
+    public String TenConThu;
+    public Integer  Hinh;
+
+    public ConThu(String tenConThu, Integer hinh) {
+        TenConThu = tenConThu;
+        Hinh = hinh;
+    }
+}
